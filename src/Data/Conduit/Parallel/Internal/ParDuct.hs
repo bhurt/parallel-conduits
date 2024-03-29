f77e49e6637b9c3638ad11e2a086fc0ac6ef4a15
Brian Hurt <bhurt42@gmail.com>
2024-03-29 16:32:05 -0500

    Complete ParDuct docu.
    By copying stuff over from Duct.

--- src/Data/Conduit/Parallel/Internal/ParDuct.hs
+++ src/Data/Conduit/Parallel/Internal/ParDuct.hs
@@ -48,18 +48,37 @@ module Data.Conduit.Parallel.Internal.ParDuct(
     import           Data.Conduit.Parallel.Internal.Spawn
     import           Data.Conduit.Parallel.Internal.Utils
 
-
+    -- | Create a new empty Duct.
     newDuct :: forall a m x . MonadIO m => Control x m (Duct.Duct a)
     newDuct = liftIO $ Duct.newDuct
 
+    -- | Create a new duct that already holds a value.
     newFullDuct :: forall a m x . MonadIO m => a -> Control x m (Duct.Duct a)
     newFullDuct a = liftIO $ Duct.newFullDuct a
 
+    -- | Allow reading from a ReadDuct.
+    --
+    -- Converts a ReadDuct into a function that reads values from that
+    -- read duct.  This function uses the standard Haskell with*
+    -- pattern for wrapping `Control.Exception.bracket`.  When the
+    -- wrapped computation exits (either via normal value return or
+    -- due to an exception), the open count of the read duct is
+    -- decremented.  If it drops to zero, then the read is closed.
+    -- 
     withReadDuct :: forall a .  Duct.ReadDuct a -> Worker (Reader a)
     withReadDuct rd = do
         r :: IO (Maybe a) <- ContT $ Duct.withReadDuct rd Nothing
         pure $ MaybeT r
 
+    -- | Allow writing to a WriteDuct.
+    --
+    -- Converts a WriteDuct into a function that writes values to that
+    -- write duct.  This function uses the standard Haskell with*
+    -- pattern for wrapping `Control.Exception.bracket`.  When the
+    -- wrapped computation exits (either via normal value return or
+    -- due to an exception), the open count of the write duct is
+    -- decremented.  If it drops to zero, then the duct is closed.
+    -- 
     withWriteDuct :: forall a . Duct.WriteDuct a -> Worker (Writer a)
     withWriteDuct wd = do
         wio :: (a -> IO Duct.Open) <- ContT $ Duct.withWriteDuct wd Nothing
@@ -71,6 +90,18 @@ module Data.Conduit.Parallel.Internal.ParDuct(
                     Duct.Closed -> pure Nothing
         pure wm
 
+    -- | Add opens to a read duct.
+    --
+    -- Read ducts have an open count associated with them.  When multiple
+    -- threads are expected to share a read duct (for example, in
+    -- `Data.Conduit.Parallel.Internal.Parallel.parallel`), you can add
+    -- to the count of opens expected. This way, the last open to exit
+    -- closes the read duct, not the first.
+    --
+    -- Note that read ducts start with an open count of 1.  So, if you
+    -- are going to have N threads use the read duct, you need to
+    -- add N-1 extra opens.
+    --
     addReadOpens :: forall a m x .
                     MonadIO m
                     => Duct.ReadDuct a
@@ -78,6 +109,17 @@ module Data.Conduit.Parallel.Internal.ParDuct(
                     -> Control x m ()
     addReadOpens rd n = liftIO $ Duct.addReadOpens rd n
 
+    -- | Add opens to a write duct.
+    --
+    -- Write ducts have an open count associated with them.  When multiple
+    -- threads are expected to share a write duct (for example, in
+    -- `Data.Conduit.parallel`), you can add to the count of opens expected.
+    -- This way, the last open to exit closes the write duct, not the first.
+    --
+    -- Note that write ducts start with an open count of 1.  So, if you
+    -- are going to have N threads use the write duct, you need to
+    -- add N-1 extra opens.
+    --
     addWriteOpens :: forall a m x .
                         MonadIO m
                         => Duct.WriteDuct a
